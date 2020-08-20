22d10c125f567a5cf5e9899cd698ac938b4eedcf
Mortensen Tomas IT UTV-3-Ö <Tomas.Mortensen@scb.se>
2020-08-20 12:54:14 +0200

    Handle default port if not set - needs to be above port 1024 or needs root on linux.

--- code/Jedi.Web/Program.cs
+++ code/Jedi.Web/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Jedi.Web
 {
@@ -20,7 +15,9 @@ namespace Jedi.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + port);
                 });
     }
 }
