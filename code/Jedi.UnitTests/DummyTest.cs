113b733aa7eec9979331d4e1e9cc9c545afcdd05
Mortensen Tomas IT UTV-3-Ö <Tomas.Mortensen@scb.se>
2020-08-20 14:46:55 +0200

    Added xunit testing and a dummy test

---
+++ code/Jedi.UnitTests/DummyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Jedi.UnitTests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
